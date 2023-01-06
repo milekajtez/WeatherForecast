3c60b2ad3fc57db6fecce19a002ca1f037832a3f
milekajtez <milekajtez@gmail.com>
2023-01-06 17:50:42 +0100

    Delete service backend

--- weather-forecast-service/models/Temperature.py
+++
@@ -1,6 +0,0 @@
-class Temperature(object):
-    def __init__(self, y2018, y2019, y2020, y2021):
-        self.y2018 = y2018
-        self.y2019 = y2019
-        self.y2020 = y2020
-        self.y2021 = y2021
--- weather-forecast-service/weather_api/controllers/test_controller.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-
-@router.get("", tags=['test'])
-async def get_test_message():
-    return 'This is test message'
